8d85eac08b6b16fd4b62fedb5d40e7441078c419
Vasil Nikolov <va.nik@hotmail.com>
2022-03-18 15:05:33 +0000

    Testing

--- WebApp-GitHub/Startup.cs
+++ WebApp-GitHub/Startup.cs
@@ -17,7 +17,7 @@ namespace WebApp_GitHub
         {
             Configuration = configuration;
         }
-
+        //Test
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
